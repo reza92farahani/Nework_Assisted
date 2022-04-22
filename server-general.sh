79fc159a7a266ce90dacdbbf6ead0bafe8fe5913
Reza Farahani <56598479+reza92farahani@users.noreply.github.com>
2022-04-22 20:49:58 +0200

    Update server-general.sh

--- server-general.sh
+++ server-general.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get -y --force-yes update
-sudo apt-get -y --force-yes vim screen install apache2 python-pip python-dev build-essential libssl-dev libffi-dev
+sudo apt-get -y --force-yes install apache2 build-essential 
